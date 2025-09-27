0c40849503df9cce408fae5e57dbb257caf87505
Asad Rasheed <itxasad98@gmail.com>
2025-09-27 11:20:17 -0700

    Added rider dashboard components

--- src/pages/Rider/RiderPage.jsx
+++ src/pages/Rider/RiderPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Sidebar from '../../components/Sidebar';
+import Topbar from '../../components/Topbar';
+import DeliveryManagement from '../../components/DeliveryManagement';
+import EarningsPayments from '../../components/EarningsPayments';
+import SupportFeedback from '../../components/SupportFeedback';
+import ProfileSettings from '../../components/ProfileSettings';
 
 const RiderPage = () => {
-  return (
-    <div>RiderPage</div>
-  )
-}
+  const [activeTab, setActiveTab] = useState('delivery');
 
-export default RiderPage
+  const renderComponent = () => {
+    switch (activeTab) {
+      case 'delivery':
+        return <DeliveryManagement />;
+      case 'earnings':
+        return <EarningsPayments />;
+      case 'feedback':
+        return <SupportFeedback />;
+      case 'profile':
+        return <ProfileSettings />;
+      default:
+        return <DeliveryManagement />;
+    }
+  };
+
+  return (
+    <div className="flex h-screen">
+      {/* Left Sidebar */}
+      <Sidebar setActiveTab={setActiveTab} />
+
+      {/* Right main area */}
+      <div className="flex flex-col flex-1">
+        <Topbar />
+        <div className="p-6 overflow-y-auto flex-1 bg-gray-50">
+          {renderComponent()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RiderPage;
